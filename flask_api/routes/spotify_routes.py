30501d32e441de9d6761a0f54d85af08cc6e6960
Martin Herbert <martinherbert93@gmail.com>
2020-05-25 23:14:16 -0400

    Close connection once we are done with it.

--- flask_api/routes/spotify_routes.py
+++ flask_api/routes/spotify_routes.py
@@ -13,17 +13,19 @@ DB_USER = os.getenv("DB_USER")
 DB_PW = os.getenv("DB_PW")
 DB_HOST = os.getenv("DB_HOST")
 
-pg_conn = psycopg2.connect(
-    dbname=DB_NAME, user=DB_USER,
-    password=DB_PW, host=DB_HOST
-    )
-pg_curs = pg_conn.cursor()
-
-
 @spotify_routes.route("/")
 def index(): #test our DB connection and verify we can pull data out in json format.
+    pg_conn = psycopg2.connect(
+        dbname=DB_NAME, user=DB_USER,
+        password=DB_PW, host=DB_HOST
+    )
+    pg_curs = pg_conn.cursor()
+
     query = '''SELECT * FROM tracks LIMIT 5'''
 
     pg_curs.execute(query)
     result = pg_curs.fetchall()
+
+    pg_curs.close()
+    pg_conn.close()
     return jsonify(result)
