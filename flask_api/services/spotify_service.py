15545275df11a161551a27a735165c9723bc7729
Martin Herbert <martinherbert93@gmail.com>
2020-05-25 21:58:38 -0400

    Csv containing filtered data.

--- flask_api/services/spotify_service.py
+++ flask_api/services/spotify_service.py
@@ -1,8 +1,12 @@
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
+from dotenv import load_dotenv
+import os
 
-client_id = "733b06048d3b42fb8ef442596c6f3ff3"
-client_secret = "01513b28eef64193b2ffac417e6b7ebb"
+load_dotenv()
+
+client_id = os.getenv("client_id")
+client_secret = os.getenv("client_secret")
 
 def spotify_api():
     client_credentials_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
