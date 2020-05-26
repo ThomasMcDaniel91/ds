4936cbb5873f0884d1e16267536f88ec99cd3833
ThomasMcDaniel91 <thomasm9105@gmail.com>
2020-05-26 17:06:07 -0500

    added dummy data path

--- flask_api/routes/spotify_routes.py
+++ flask_api/routes/spotify_routes.py
@@ -29,3 +29,38 @@ def index(): #test our DB connection and verify we can pull data out in json for
     pg_curs.close()
     pg_conn.close()
     return jsonify(result)
+
+@spotify_routes.route("/dummy_data")
+def dummy_data(): #just some dummy data to get working with.
+  dummy_list = [
+            {'title': 'The Ocean - 1990 Remaster',
+            'album_name': 'Houses of the Holy (1994 Remaster)',
+            'artist': 'Led Zeppelin',
+            'album_art': 'https://i.scdn.co/image/ab67616d0000b273441fd03f69579d36801631d9'
+            },
+            
+            {'title': 'I Disappear',
+            'album_name': 'I Disappear',
+            'artist': 'Metallica',
+            'album_art': 'https://i.scdn.co/image/ab67616d0000b273bfe41664b2ca1038b5e3bc6c'
+            },
+
+            {'title': 'Creeping Death (Remastered)',
+            'album_name': 'Ride The Lightning (Deluxe Remaster)',
+            'artist': 'Metallica',
+            'album_art': 'https://i.scdn.co/image/ab67616d0000b273533fd0b248052d04e6b732c0'
+            },
+
+            {'title': "I Guess That's Why They Call It The Blues",
+            'album_name': 'Too Low For Zero',
+            'artist': 'Elton John',
+            'album_art': 'https://i.scdn.co/image/ab67616d0000b273eb11e2abccdca41f39ad3b89'
+            },
+
+            {'title': 'Sweet Leaf - 2014 Remaster',
+            'album_name': 'Master of Reality (2014 Remaster)',
+            'artist': 'Black Sabbath',
+            'album_art': 'https://i.scdn.co/image/ab67616d0000b273c199494ba9ea2b73e9208f91'
+            }
+  ]
+  return jsonify(dummy_list)
